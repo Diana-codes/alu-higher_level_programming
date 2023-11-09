5a040a0419eab241fba5797f78fb3faa72490979
Diana-codes <dianaruzindana8@gmail.com>
2023-11-09 05:26:58 -0800

    1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    if not matrix:
-        return None
-    return [[num * num for num in row] for row in matrix]
+def search_replace(my_list, search, replace):
+    new_list = []
+    new_list = [x if x != search else replace for x in my_list]
+    return new_list
