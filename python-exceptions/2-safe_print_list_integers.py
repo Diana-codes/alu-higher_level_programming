cab9844aec15ba1178968933b858bbe84b079a25
Diana-codes <dianaruzindana8@gmail.com>
2023-11-10 01:03:00 -0800

    2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
-    try:
-        result = a / b
-    except:
-        result = None
-    finally:
-        print("Inside result: {}".format(result))
-        return (result)
+def safe_print_list_integers(my_list=[], x=0):
+    printed_nums = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            printed_nums += 1
+        except (TypeError, ValueError):
+            continue
+    print()
+    return printed_nums
